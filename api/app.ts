e65ce51397f71c5641edf6107245d30aa40d5fba
Maxim Ten <maximten94@gmail.com>
2022-04-03 14:43:11 +0600

    rates and sums

--- api/app.ts
+++ api/app.ts
@@ -22,8 +22,8 @@ const balanceTotalSchema = new mongoose.Schema({
     day: Number,
     month: Number,
     year: Number,
-    symbol: String,
-    isConvertedTotal: Boolean
+    sumUSD: mongoose.Types.Decimal128,
+    sums: [{ symbol: String, sum: mongoose.Types.Decimal128 }]
 })
 
 const Transaction = mongoose.model("Transaction", transactionSchema)
@@ -57,6 +57,19 @@ const getTransactions = () => {
     return transactions
 }
 
+const getExchangesKZT = async () => {
+    const data: {
+        Date: string,
+        Close: string
+    }[] = require('../usdkzt.json')
+    const rates = data.map(row => ({
+        date: new Date(row["Date"]),
+        symbol: "KZT",
+        rate: row["Close"]
+    }))
+    await ExchangeRateUSD.insertMany(rates)
+}
+
 const getDateRange = async () => {
     const [{max, min}] = await Transaction.aggregate([{ $group: { _id: null, max: { $max: "$date" }, min: { $min: "$date" } } }])
     return {max, min}
@@ -74,6 +87,12 @@ const getDatePlusOne = (date: Date) => {
     return datePlusOne
 }
 
+const getDateMinusTwo = (date: Date) => {
+    const dateMinusTwo = new Date(date)
+    dateMinusTwo.setDate(dateMinusTwo.getDate() - 2)
+    return dateMinusTwo 
+}
+
 const getDayTransactions = async (date: Date) => {
     const datePlusOne = getDatePlusOne(date)
     return await Transaction.find({
@@ -83,10 +102,41 @@ const getDayTransactions = async (date: Date) => {
 
 const getDaySum = async (date: Date) => {
     const datePlusOne = getDatePlusOne(date)
-    return await Transaction.aggregate([
+    const sumsData: {
+        _id: string,
+        sum: number
+    }[] = await Transaction.aggregate([
         { $match: { date: { $gte: date, $lt: datePlusOne }}},
         { $group: { _id: "$symbol", sum: {$sum: "$amount"} }}
     ])
+    const sums = sumsData.map(sum => ({symbol: sum._id, sum: sum.sum}))
+    const sumsUSD = await Promise.all(sums.map(async (sum) => {
+        if (sum.symbol === "USD") {
+            return sum
+        } else {
+            const newSum = {...sum}
+            const exchangeRate = await getSymbolRateAtDate(date, sum.symbol)
+            newSum.sum = sum.sum / exchangeRate
+            return newSum
+        }
+    }))
+    const sumUSD = sumsUSD.reduce((carry, sum) => carry + sum.sum, 0)
+    return { sums, sumUSD }
+}
+
+const saveDaySum = async (date: Date, { sums, sumUSD }: { sums: {
+    symbol: string,
+    sum: number
+}[], sumUSD: number }) => {
+    const total = new BalanceTotal({
+        dateType: "day",
+        day: date.getDate(),
+        month: date.getMonth(),
+        year: date.getFullYear(),
+        sumUSD,
+        sums
+    })
+    await total.save()
 }
 
 const getDayDetailing = async (date: Date) => {
@@ -112,20 +162,33 @@ const getMonthDetailing = async (year: number, month: number) => {
     ])
 }
 
-const init = async () => {
+const getSymbolRateAtDate = async (date: Date, symbol: string) => {
+    const dateMinusTwo = getDateMinusTwo(date)
+    const rates = await ExchangeRateUSD.find({
+        date: {$gte: dateMinusTwo, $lte: date},
+        symbol 
+    }).sort({date: -1})
+    if (rates.length < 1) {
+        // TODO: API CALL
+    } else {
+        return rates[0].rate
+    }
+}
+
+const initApp = async () => {
     const app = express()
     app.use(bodyParser.json())
     app.use(cors())
     await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost/scrooge`)
     app.get("/range", async (req, res) => {
-        const monthRange = await getMonthRange()    
+        const monthRange = await getMonthRange()
         res.send(monthRange)
     })
     app.get("/month", async (req, res) => {
-        const {year, month} = req.query
+        const { year, month } = req.query
         if (!year || !month) {
             res.status(400).end()
-        } 
+        }
         const yearNum = Number.parseInt(year as string)
         const monthNum = Number.parseInt(month as string)
         if (Number.isNaN(yearNum) || Number.isNaN(monthNum)) {
@@ -137,6 +200,24 @@ const init = async () => {
     app.listen(8080, () => {
         console.log('listening on 8080')
     })
+}
+
+const migrate = async () => {
+    await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost/scrooge`)
+    const rate = await getExchangesKZT()
+}
+
+const test = async () => {
+    await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost/scrooge`)
+    const date = new Date(2022, 2, 3)
+    const sums = await getDaySum(date)
+    await saveDaySum(date, sums)
+}
+
+const init = async () => {
+    // await initApp()
+    // await migrate()
+    await test()
 }
 
 init().catch(err => console.error(err))
